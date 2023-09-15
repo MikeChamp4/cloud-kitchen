e37d1442beed464cc910559ef157a89484bb5986
Miguel Enrique Zambrano Contreras <miguel.zambrano@inetum.com>
2023-09-15 09:36:23 +0200

    order service finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from './../../services/order-details.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+  foodData: any;
+
+  constructor(private service: OrderDetailsService) { }
+
+  ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
+  }
 }
